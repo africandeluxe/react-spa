bc3ee4611cd8ef21c4e5547ce9c635c24e4b69dc
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-14 18:34:22 +0200

    CSS layout and styling on components + added data.js file with content data

---
+++ my-spa/src/data.js
@@ -0,0 +1,19 @@
+const contentData = {
+  Home: {
+    title: "Welcome to Our Blog!",
+    text: "This is our fantastic home page where you can find interesting insights and great tips.",
+    image: "/images/home.jpg"
+  },
+  About: {
+    title: "About Us",
+    text: "Learn more about our team and mission. Our blog is dedicated to bringing you the latest and greatest in the world of tech.",
+    image: "/images/about.jpg"
+  },
+  Contact: {
+    title: "Contact Us",
+    text: "Have any questions? Reach out to us via the contact information below. We'd love to hear from you!",
+    image: "/images/contact.jpg"
+  }
+};
+
+export default contentData;
