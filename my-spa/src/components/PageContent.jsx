33b996ee5da8a27198d1dcf5d3334ff4177d6018
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-14 18:53:49 +0200

    Map method for rendering components

--- my-spa/src/components/PageContent.jsx
+++ my-spa/src/components/PageContent.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import contentData from '../data';
+import contentData from '../data/data';
+import posts from '../data/postsData';
 
 const PageContent = ({ currentPage }) => {
   const content = contentData[currentPage];
+
+  const renderPosts = () => {
+    return posts.map(post => (
+      <div key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.summary}</p>
+      </div>
+    ));
+  };
 
   return (
     <div className="content">
       <h1>{content.title}</h1>
       <p>{content.text}</p>
+      {currentPage === 'Home' && renderPosts()}
     </div>
   );
 };
---
+++ my-spa/src/data/postsData.js
@@ -0,0 +1,7 @@
+const posts = [
+  { id: 1, title: "Blog Post 1", summary: "Summary of blog post 1..." },
+  { id: 2, title: "Blog Post 2", summary: "Summary of blog post 2..." },
+  { id: 3, title: "Blog Post 3", summary: "Summary of blog post 3..." }
+];
+
+export default posts;
